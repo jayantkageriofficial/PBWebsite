420b90e5283f9d4fe4d142f6289e28c7e66d9e08
Shaurya Bisht <shauryabisht707@gmail.com>
2026-04-11 01:01:44 +0530

    bugfix

--- lib/db/connection.ts
+++ lib/db/connection.ts
@@ -1,41 +1,41 @@
-// import mongoose from "mongoose";
+import mongoose from "mongoose";
 
-// const MONGODB_URI = process.env.MONGODB_URI!;
+const MONGODB_URI = process.env.MONGODB_URI!;
 
-// if (!MONGODB_URI) {
-//   throw new Error("MONGODB_URI environment variable is not defined");
-// }
+if (!MONGODB_URI) {
+  throw new Error("MONGODB_URI environment variable is not defined");
+}
 
-// let cached = global as typeof global & {
-//   mongoose?: {
-//     conn: typeof mongoose | null;
-//     promise: Promise<typeof mongoose> | null;
-//   };
-// };
+let cached = global as typeof global & {
+  mongoose?: {
+    conn: typeof mongoose | null;
+    promise: Promise<typeof mongoose> | null;
+  };
+};
 
-// if (!cached.mongoose) {
-//   cached.mongoose = { conn: null, promise: null };
-// }
+if (!cached.mongoose) {
+  cached.mongoose = { conn: null, promise: null };
+}
 
-// async function connectDB() {
-//   if (cached.mongoose!.conn) {
-//     console.info("Using cached MongoDB connection");
-//     return cached.mongoose!.conn;
-//   }
+async function connectDB() {
+  if (cached.mongoose!.conn) {
+    console.info("Using cached MongoDB connection");
+    return cached.mongoose!.conn;
+  }
 
-//   if (!cached.mongoose!.promise)
-//     cached.mongoose!.promise = mongoose.connect(MONGODB_URI).then((m) => m);
+  if (!cached.mongoose!.promise)
+    cached.mongoose!.promise = mongoose.connect(MONGODB_URI).then((m) => m);
 
-//   cached.mongoose!.conn = await cached.mongoose!.promise;
+  cached.mongoose!.conn = await cached.mongoose!.promise;
 
-//   console.info("Established new MongoDB connection");
-//   return cached.mongoose!.conn;
-// }
+  console.info("Established new MongoDB connection");
+  return cached.mongoose!.conn;
+}
 
-// if (process.env.NEXT_RUNTIME === "nodejs") {
-//   connectDB().catch((err) => {
-//     console.error("Failed to connect to MongoDB:", err);
-//   });
-// }
+if (process.env.NEXT_RUNTIME === "nodejs") {
+  connectDB().catch((err) => {
+    console.error("Failed to connect to MongoDB:", err);
+  });
+}
 
-// export default connectDB;
+export default connectDB;
