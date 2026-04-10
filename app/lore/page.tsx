045b5ba1f3b6f70b36964fb11d674d66e3e3e7a3
Jayant Hegde Kageri <jayantkageri@gmail.com>
2026-04-10 19:00:09 +0530

    Homepage Structure

--- app/lore/page.tsx
+++ app/lore/page.tsx
@@ -1,22 +1,10 @@
-import connectDB from '@/db/connection'
-import { Metadata } from 'next'
+import { Metadata } from "next";
 
-connectDB()
+export const metadata: Metadata = {
+  title: "Lore",
+  description: "The Lores of Point Blank",
+};
 
-
-export const metadata:Metadata  = {
-    title:"Lore",
-    description:"The Lores of Point Blank"
-  } 
-
-
-
-export default async function Lore() {  
-  return (
-    <div>
-      
-    </div>
-  )
+export default async function Lore() {
+  return <div></div>;
 }
-
-
