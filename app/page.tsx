089be1d06e78973578982056310cc71959772ebc
Jayant Hegde Kageri <jayantkageri@gmail.com>
2026-04-26 02:36:31 +0530

    navbar bg while transitioning

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { useEffect } from "react";
 import HeroSection from "@/components/homepage/HeroSection";
 import MissionVisionSection from "@/components/homepage/MissionVisionSection";
 import CardStack from "@/components/homepage/CardStack";
@@ -5,8 +8,16 @@ import DomainsSection from "@/components/homepage/DomainsSection";
 import ActivitiesSection from "@/components/homepage/ActivitiesSection";
 import FoundingMembersSection from "@/components/homepage/FoundingMembersSection";
 import StayConnectedSection from "@/components/homepage/StayConnectedSection";
+import { useLoadingStore } from "@/lib/store/loading";
 
 export default function Home() {
+  const setLoading = useLoadingStore((s) => s.setLoading);
+
+  useEffect(() => {
+    setLoading(false);
+    return () => setLoading(true);
+  }, [setLoading]);
+
   return (
     <>
       <HeroSection />
