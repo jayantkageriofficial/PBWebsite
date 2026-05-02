0845ad612b3d923e3188ee3655212e25969369be
yugeshweb <yugeshweb@gmail.com>
2026-05-02 15:16:56 +0530

    Fixed scroll loading

--- lib/db/connection.ts
+++ lib/db/connection.ts
@@ -1,4 +1,9 @@
 import mongoose from "mongoose";
+import dns from "dns";
+
+if (typeof window === "undefined") {
+  dns.setServers(["8.8.8.8", "8.8.4.4"]);
+}
 
 const MONGODB_URI = process.env.MONGODB_URI!;
 
